33b0eded4ffa9ff3e95d1345c10643e771de8524
Chris Kelker <chris@wecreatedigital.co.uk>
2019-02-23 16:29:48 +0000

    Completed Profile Page

--- app/Recipe.php
+++ app/Recipe.php
@@ -52,4 +52,24 @@ class Recipe extends Model
     {
         return $this->hasMany('App\Review', 'recipe_id');
     }
+
+    /**
+     * [rating description]
+     * Determine Overall Recipe Rating by all recipes
+     *
+     * @author  Christopher Kelker
+     * @version 1.0.0
+     * @date    2019-02-23
+     * @return  [type]
+     */
+    public function rating()
+    {
+        // Get Array of Ratings
+        $array = $this->reviews()->pluck('rating')->toArray();
+
+        //Calculate the average.
+        $average = round(array_sum($array) / count($array));
+
+        return $average;
+    }
 }
