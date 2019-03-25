2ae98cec1b8a9854d4297395e473a0989d2cd07d
Chris Kelker <chris@wecreatedigital.co.uk>
2019-03-25 23:49:25 +0000

    WIP: New login page styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,76 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+  <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+      <div class="col-md-8">
+        <div class="card">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+          <div class="card-body">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            <div class="row">
+              <div class="col text-center">
+                  <h2 class="section-title mb-3"><span>Sign In</span> With</h2>
+              </div>
             </div>
+
+            <form method="POST" action="{{ route('login') }}">
+              @csrf
+
+              <div class="row mb-4">
+                <div class="col text-center">
+                    <button type="button" class="btn btn-light"><i class="fab fa-facebook-square"></i> Facebook</button>
+                    <button type="button" class="btn btn-light"><i class="fab fa-google"></i> Google</button>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label for="exampleInputEmail1">{{ __('Email/Username') }}</label>
+                <input id="email" type="email" class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+
+                @if ($errors->has('email'))
+                  <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('email') }}</strong>
+                  </span>
+                @endif
+              </div>
+
+              <div class="form-group">
+                <div class="d-flex flex-row justify-content-start">
+                    <label for="password" class="mr-2">{{ __('Password') }}</label>
+                    @if (Route::has('password.request'))
+                      <a class="" href="{{ route('password.request') }}">
+                        <small>{{ __('Forgot?') }}</small>
+                      </a>
+                    @endif
+                  </div>
+                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                @if ($errors->has('password'))
+                  <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('password') }}</strong>
+                  </span>
+                @endif
+              </div>
+
+              <div class="form-group row mb-3">
+                <div class="col">
+                  <button type="submit" class="btn btn-primary btn-block">
+                    {{ __('Login') }}
+                  </button>
+                </div>
+              </div>
+            </form>
+
+            <div class="row mb-0">
+              <div class="col text-center">
+
+                <small class="text-muted">Not a member? <a href="{{ route('register') }}">Sign up now</a></small>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -1,15 +1,10 @@
-<section class="section about">
+<section class="about-section">
   <div class="container">
-
     <div class="row">
-      <div class="col">
-        <div class="text-center">
-          <h2 class="text-primary">Who we are</h2>
-          <hr class="mt-0">
-          <p class="mb-3">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum blanditiis praesentium itaque debitis iure dolore, dicta perferendis molestiae id facere.
-          </p>
-        </div>
+      <div class="col text-center">
+          <h2 class="section-title mb-2"><span>Sharing</span> never became so easy</h2>
+          <hr class="mt-0 w-50">
+        <h5 class="mb-5">This is the paragraph where you can write more details about your team. Keep you user engaged by providing meaningful information.</h5>
       </div>
     </div>
 
@@ -17,15 +12,13 @@
       <div class="row">
         <div class="col-lg-3 col-md-6 col-sm-12 mb-6">
           <div class="item">
-            <div class="item-img">
+            <div class="item-img mb-3">
               <a href="#" title="">
-                <img class="img-fluid" src="img/intro-blue-icon1.png" alt="img">
+                <img class="rounded-circle img-fluid" src="https://source.unsplash.com/900x900/?food,recipes" alt="img">
               </a>
             </div>
             <div class="item-content">
-              <div class="sub-title">
-                <h4>Natural Farm</h4>
-              </div>
+              <h4 class="section-title mb-2"><span>Natural</span> Farm</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipiscing elit felis euismod sempe.</p>
             </div>
           </div>
@@ -33,47 +26,41 @@
 
         <div class="col-lg-3 col-md-6 col-sm-12 mb-6">
           <div class="item">
-            <div class="item-img">
+            <div class="item-img mb-3">
               <a href="#" title="">
-                <img class="img-fluid" src="img/intro-blue-icon2.png" alt="img">
+                <img class="rounded-circle img-fluid" src="https://source.unsplash.com/901x901/?food,recipes" alt="img">
               </a>
             </div>
             <div class="item-content">
-              <div class="sub-title">
-                <h4>Healthy Food</h4>
-              </div>
+              <h4 class="section-title mb-2"><span>Healthy</span> Food</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipiscing elit felis euismod sempe.</p>
             </div>
           </div>
         </div>
 
-        <div class="col-lg-3 col-md-6 col-sm-12 m-b-50">
+        <div class="col-lg-3 col-md-6 col-sm-12 mb-6">
           <div class="item">
-            <div class="item-img">
+            <div class="item-img mb-3">
               <a href="#" title="">
-                <img class="img-fluid" src="img/intro-blue-icon3.png" alt="img">
+                <img class="rounded-circle img-fluid" src="https://source.unsplash.com/902x902/?food,recipes" alt="img">
               </a>
             </div>
             <div class="item-content">
-              <div class="sub-title">
-                <h4>Conserves Biodiversity</h4>
-              </div>
+              <h4 class="section-title mb-2"><span>Conserves</span> Biodiversity</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipiscing elit felis euismod sempe.</p>
             </div>
           </div>
         </div>
 
-        <div class="col-lg-3 col-md-6 col-sm-12">
+        <div class="col-lg-3 col-md-6 col-sm-12 mb-6">
           <div class="item">
-            <div class="item-img">
+            <div class="item-img mb-3">
               <a href="#" title="">
-                <img class="img-fluid" src="img/intro-blue-icon4.png" alt="img">
+                <img class="rounded-circle img-fluid" src="https://source.unsplash.com/903x903/?food,recipes" alt="img">
               </a>
             </div>
             <div class="item-content">
-              <div class="sub-title">
-                <h4>Biologically Safe</h4>
-              </div>
+              <h4 class="section-title mb-2"><span>Biologically</span> Safe</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipiscing elit felis euismod sempe.</p>
             </div>
           </div>
