12f3673c33dc983c1917487e97fbabcd4a20b093
Chris Kelker <chris@wecreatedigital.co.uk>
2019-04-04 23:56:16 +0100

    WIP: Homepage next section - services or "what we do

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,59 +1,98 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid bg-image bg-full from-top" style="background-image: url('/images/background1.jpeg');">
+  <div class="container-fluid bg-image bg-full from-top" style="background-image: url('/images/background1.jpeg');">
     <div class="row justify-content-center h-100 align-items-center">
-        <div class="col-md-8">
-            <div class="card transparent">
-                <h1 class="home-title">Find Recipes</h1>
-                <p class="text-center font-italic">Let us help you find a recipe with what you have leftover.</p>
-                <div class="card-body">
-                    <form class="form-inline row" action="/search/results" method="get">
-                      <div class="col-12 input-group">
-                        <label for="ingredients" class="sr-only">Ingredients</label>
-                        <select name="ingredients[]" multiple="multiple" class="form-control-plaintext tags-ingredients" id="ingredients">
-                            <option>Search ingredients in your cupboard...</option>
-                            @foreach($ingredients as $ingredient)
-                                <option value="{{{ $ingredient->id }}}">{{{ $ingredient->value }}}</option>
-                            @endforeach
-                        </select>
-                        <button type="submit" class="btn btn-primary ml-3">Find receipes</button>
-                      </div>
-                    </form>
+      <div class="col-md-8">
+        <div class="card transparent">
+          <h1 class="home-title">Find Recipes</h1>
+          <p class="text-center font-italic">Let us help you find a recipe with what you have leftover.</p>
+          <div class="card-body">
+            <form class="form-inline row" action="/search/results" method="get">
+              <div class="col-12 input-group mb-3">
+                <input type="text" class="form-control" placeholder="Author's name or Recipe name..." aria-label="Author's name or Recipe name...">
+                <div class="input-group-append">
+                  <button class="btn btn-primary" type="button">{{ __('Find Recipes') }}</button>
                 </div>
-            </div>
+              </div>
+            </form>
+          </div>
         </div>
-    </div>
-</div>
-
-
-<section class="popular-recipes-section">
-  <div class="container my-5 py-5">
-    <div class="row mb-5">
-      <div class="col text-center">
-        <h2 class="section-title mb-3"><span>Most Popular</span> Recipes</h2>
-        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
       </div>
     </div>
+  </div>
 
-    <div class="row justify-content-center">
-      @each('recipes.sections.individual-recipe-card', $popular_recipes, 'recipe')
+
+  <section class="popular-recipes-section">
+    <div class="container my-5 py-5">
+      <div class="row mb-5">
+        <div class="col text-center">
+          <h2 class="section-title mb-3"><span>Most Popular</span> Recipes</h2>
+          <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+      </div>
+
+      <div class="row justify-content-center">
+        @each('recipes.sections.individual-recipe-card', $popular_recipes, 'recipe')
+      </div>
     </div>
-  </div>
-</section>
+  </section>
 
 
 
 
 
-<section class="content-section">
-  <div class="container">
-     <div class="row">
-       <div class="col">
+  <section class="content-section">
+    <div class="container my-5 py-5">
+      <div class="row justify-content-center">
+        <div class="col-md-4">
+          <div class="card card-img-top-canvas card-header-bounce bg-shadow">
+            <div class="card-header card-header-image">
+              <img src="/images/colourful_image1.jpeg" alt="">
+              <div class="colored-shadow" style="background-image: url(/images/colourful_image1.jpeg); opacity: 1;">
+              </div>
+            </div>
+            <div class="card-body">
+              <h6 class="card-category text-primary">Lorem ipsum.</h6>
+              <h4 class="card-title">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </h4>
+            </div>
+          </div>
+        </div>
 
-       </div>
-     </div>
-  </div>
-</section>
+        <div class="col-md-4">
+          <div class="card card-img-top-canvas card-header-bounce bg-shadow">
+            <div class="card-header card-header-image">
+              <img src="/images/colourful_image2.jpeg" alt="">
+              <div class="colored-shadow" style="background-image: url(/images/colourful_image2.jpeg); opacity: 1;">
+              </div>
+            </div>
+            <div class="card-body">
+              <h6 class="card-category text-primary">Lorem ipsum.</h6>
+              <h4 class="card-title">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </h4>
+            </div>
+          </div>
+        </div>
 
+        <div class="col-md-4">
+          <div class="card card-img-top-canvas card-header-bounce bg-shadow">
+            <div class="card-header card-header-image">
+              <img src="/images/colourful_image3.jpeg" alt="">
+              <div class="colored-shadow" style="background-image: url(/images/colourful_image3.jpeg); opacity: 1;">
+              </div>
+            </div>
+            <div class="card-body">
+              <h6 class="card-category text-primary">Lorem ipsum.</h6>
+              <h4 class="card-title">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 @endsection
--- resources/views/recipes/sections/individual-recipe-card.blade.php
+++ resources/views/recipes/sections/individual-recipe-card.blade.php
@@ -1,12 +1,12 @@
 <div class="col-lg-4 col-12 mb-4">
-  <div class="card card-recipe card-bounce card-hover bg-shadow">
+  <div class="card card-recipe bounce card-hover bg-shadow">
     <div class="position-relative">
       <a href="{{ $recipe->path() }}">
-        <img class="card-img-top" src="https://source.unsplash.com/1600x900/?recipes,food" alt="Card image cap">
+        <img class="card-img-top" src="/images/placeholder-recipe-image.jpeg" alt="Card image cap">
       </a>
       @if (Auth::check())
         <div class="position-absolute heart-amount">
-          <button class="btn btm-sm btn-light" type="button" name="button">
+          <button class="btn btm-sm btn-light" type="button" name="button" data-toggle="tooltip" data-placement="top" title="Like">
             <i class="fas fa-heart text-red"></i> {{ $recipe->likes_count }}
           </button>
         </div>
@@ -15,7 +15,7 @@
 
       @if( !empty($recipe->user->avatar))
         <div class="position-absolute user-avatar">
-            <a href="#" class="view-user">
+            <a href="#" class="view-user" data-toggle="tooltip" data-placement="top" title="View profile">
               <div class="d-flex align-items-center flex-row">
                 <img src="{{ url($recipe->user->avatar) }}" class="dropdown-toggle-avatar">
                 <p class="mb-0 ml-2 text-white">{{ $recipe->user->at_username }}</p>
@@ -25,7 +25,7 @@
       @endif
 
       <div class="position-absolute download-button">
-        <button class="btn btn-social btn-light text-dark d-flex justify-content-center align-items-center m-0" type="button" name="button" data-toggle="tooltip" data-placement="top" title="Click to download!">
+        <button class="btn btn-social btn-light text-dark d-flex justify-content-center align-items-center m-0" type="button" name="button" data-toggle="tooltip" data-placement="top" title="Download">
           <i class="fas fa-download"></i>
         </button>
       </div>
